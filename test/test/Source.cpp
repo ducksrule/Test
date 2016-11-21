5f35757cb93dcddae1ea83711b06210a5842b42a
Name <email@email.com>
2016-11-21 11:09:59 -0500

    Made a ducky function

--- test/test/Source.cpp
+++ test/test/Source.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main() {
 	cout << "This is my change" << endl;
 
+
+	int getDucky = duckyFunction();
+
 	return 0;
+}
+
+int duckyFunction() {
+
+	//do stuff
+	return 1;
 }
